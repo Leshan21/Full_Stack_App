7c20ba762441ae33b77e8b6673ac04a00dfe93c5
Leshan <leshanpasindu12@gmail.com>
2025-11-04 18:57:28 +0530

    create simple get api for testing purpose

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('server is ready')
+})
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })

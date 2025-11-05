2c71aa1de6046ee37daa5c467c89803d6cc93f73
Leshan <leshanpasindu12@gmail.com>
2025-11-05 08:42:14 +0530

    create simple get req route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ dotenv.config();
 
 const app = express();
 
+// route
+app.get('/', (req, res) => {
+  res.send('API is running.....');
+})
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT} `);
 })

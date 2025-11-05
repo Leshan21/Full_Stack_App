953edf97d10601db4039080645846f8cc6468032
Leshan <leshanpasindu12@gmail.com>
2025-11-05 08:15:23 +0530

    strat new project

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,1 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('server is ready')
-})
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+const express = require('express');

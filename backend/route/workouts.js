a9cd93c28ffa99ce904fb397befd47cde3f36726
Leshan <leshanpasindu12@gmail.com>
2025-11-05 08:57:53 +0530

    create basic api templates

--- backend/route/workouts.js
+++ backend/route/workouts.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Get all workouts
 router.get('/', (req, res) => {
   res.send('Get all workouts');
+})
+
+// Get single workout
+router.get('/:id', (req, res) => {
+  
+})
+
+// Post a workout
+router.post('/', (req, res) => {
+  
+})
+
+// Delete a workout
+router.delete('/:id', (req, res) => {
+  
+})
+
+// Update a workout
+router.patch('/:id', (req, res) => {
+  
 })
 
 module.exports = router;
